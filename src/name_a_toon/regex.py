f96d05d906a6ff57ca4a899379699cf909d49fa6
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2023-02-02 17:51:57 -0600

    Update regex.py

--- src/name_a_toon/regex.py
+++ src/name_a_toon/regex.py
@@ -1,13 +1,5 @@
 import re
 
-title = re.compile('^[0-9]{1,4}[*]0[*]')
-
-first = re.compile('^[0-9]{1,4}[*]1[*]')
-
-last1 = re.compile('^[0-9]{1,4}[*][2-3][*]')
-
-last2 = re.compile('^[0-9]{1,4}[*]4[*]')
-
 class NameList:
     """Class that holds a list of all possible Toon Name entries."""
     def __init__(self,titles:list=[],firsts:list=[],last1s:list=[],last2s:list=[]):
