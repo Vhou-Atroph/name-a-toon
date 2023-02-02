f1c66b9f4e5e225a8037f19debb14516e8ea73c8
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2023-02-02 16:36:29 -0600

    Update gen.py
    fixing for o' names

--- src/name_a_toon/gen.py
+++ src/name_a_toon/gen.py
@@ -8,7 +8,10 @@ class Name:
         self.title = title
         self.first = first
         self.last1 = last1
-        self.last2 = last2
+        if self.last1.endswith("'"):
+            self.last2 = last2.title()
+        else:
+            self.last2 = last2
     
     def fromrand(list:NameList):
         """Generate a completely random name from a given list."""
