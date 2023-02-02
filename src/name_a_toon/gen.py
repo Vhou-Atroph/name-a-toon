f7c6394bb50d7ef96d20177be1c68c9f0dc831f1
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2023-02-02 16:15:58 -0600

    Create gen.py

---
+++ src/name_a_toon/gen.py
@@ -0,0 +1,18 @@
+import random
+
+from .regex import NameList
+
+class Name:
+    """A Name that can be given to a Toon."""
+    def __init__(self,title:str,first:str,last1:str,last2:str):
+        self.title = title
+        self.first = first
+        self.last1 = last1
+        self.last2 = last2
+    
+    def fromrand(list:NameList):
+        """Generate a completely random name from a given list."""
+        return Name(title=random.choice(list.title),
+        first=random.choice(list.first),
+        last1=random.choice(list.last1),
+        last2=random.choice(list.last2))
