87278b2bf768578b57fd41a8f0f635d19e0f1af7
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2023-02-02 16:36:09 -0600

    Update regex.py
    i sure hope this works

--- src/name_a_toon/regex.py
+++ src/name_a_toon/regex.py
@@ -10,11 +10,25 @@ last2 = re.compile('^[0-9]{1,4}[*]4[*]')
 
 class NameList():
     """Class that holds a list of all possible Toon Name entries."""
-    def __init__(self):
-        self.title = []
-        self.first = []
-        self.last1 = []
-        self.last2 = []
-    def fromfile(file:str):
-        #TODO: this
-        print("unimplemented")
+    def __init__(self,titles=list,firsts=list,last1s=list,last2s=list):
+        self.title = titles
+        self.first = firsts
+        self.last1 = last1s
+        self.last2 = last2s
+    def from_file(self,path:str):
+        file = open(path,'r')
+        text = file.readlines()
+        file.close()
+        for line in text:
+            txt = line.split("*", 2)
+            match True:
+                case re.search('^[0-9]{1,4}[*]0[*]',line):
+                    self.title.append(txt[1])
+                case re.search('^[0-9]{1,4}[*]1[*]',line):
+                    self.first.append(txt[1])
+                case re.search('^[0-9]{1,4}[*][2-3][*]',line):
+                    self.last1.append(txt[1])
+                case re.search('^[0-9]{1,4}[*]4[*]',line):
+                    self.last2.append(txt[1])
+                case _:
+                    continue
