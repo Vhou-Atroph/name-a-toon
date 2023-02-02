7e01dd34ccb26377bd46a60badf83e4de49fb270
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2023-02-02 17:02:13 -0600

    Update regex.py

--- src/name_a_toon/regex.py
+++ src/name_a_toon/regex.py
@@ -8,27 +8,35 @@ last1 = re.compile('^[0-9]{1,4}[*][2-3][*]')
 
 last2 = re.compile('^[0-9]{1,4}[*]4[*]')
 
-class NameList():
+class NameList:
     """Class that holds a list of all possible Toon Name entries."""
-    def __init__(self,titles=list,firsts=list,last1s=list,last2s=list):
+    def __init__(self,titles:list=[],firsts:list=[],last1s:list=[],last2s:list=[]):
         self.title = titles
         self.first = firsts
         self.last1 = last1s
         self.last2 = last2s
-    def from_file(self,path:str):
+    def from_file(path:str):
         file = open(path,'r')
         text = file.readlines()
         file.close()
+        title = []
+        first = []
+        last1 = []
+        last2 = []
         for line in text:
             txt = line.split("*", 2)
-            match True:
-                case re.search('^[0-9]{1,4}[*]0[*]',line):
-                    self.title.append(txt[1])
-                case re.search('^[0-9]{1,4}[*]1[*]',line):
-                    self.first.append(txt[1])
-                case re.search('^[0-9]{1,4}[*][2-3][*]',line):
-                    self.last1.append(txt[1])
-                case re.search('^[0-9]{1,4}[*]4[*]',line):
-                    self.last2.append(txt[1])
-                case _:
-                    continue
+            if re.search('^[0-9]{1,4}[*]0[*]',line):
+                title.append(txt[2].strip())
+                continue
+            elif re.search('^[0-9]{1,4}[*]1[*]',line):
+                first.append(txt[2].strip())
+                continue
+            elif re.search('^[0-9]{1,4}[*][2-3][*]',line):
+                last1.append(txt[2].strip())
+                continue
+            elif re.search('^[0-9]{1,4}[*]4[*]',line):
+                last2.append(txt[2].strip())
+                continue
+            else:
+                continue
+        return NameList(title,first,last1,last2)
